e4f6d388f19d8cd0f4625704b2815da52a67b60a
Babalpreet <babaldon1111@gmail.com>
2025-01-09 23:59:03 +0530

    palidrome check using recursion

--- recursion/sumOfNintegers.cpp
+++ recursion/sumOfNintegers.cpp
@@ -1,4 +1,18 @@
 #include<iostream>
 
 using namespace std;
-int main()
+
+int sum(int n){
+    if(n==1){
+        return 1;
+    }
+    return n+sum(n-1);
+}
+
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    cout<<"Sum of first "<<n<<" natural numbers is: "<<sum(n);
+
+}
