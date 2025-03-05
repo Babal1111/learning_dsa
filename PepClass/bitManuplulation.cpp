30791e7de4f938d98777bb95d1e3dd6a03206bcd
Babalpreet <babaldon1111@gmail.com>
2025-03-05 11:52:19 +0530

    bitmanupulation

---
+++ PepClass/bitManuplulation.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// int main(){
+//     int n =22;
+//     int num=n;
+//     string str ="";
+
+//     while(n>0){
+//         int rem = n%2;
+//         cout<<rem;
+        
+//         str+= to_string(rem);;
+        
+//         n=n/2;
+        
+         
+//     }
+//     reverse(str.begin(),str.end());
+//     cout<<"the binary representation of "<<num<<" is :"<<str;
+// }
+int main(){
+
+    string str= "1011" // ie 11
+
+    // reverse(str.begin(),str.end());
+    int power = 1;
+    int num =0;
+    for(int i =strength()-1;i>=0;i--){
+        if(str[i]=='1'){
+            num=num+pow;
+        }
+        power=power*2;
+
+    }
+cout<<"decimal no is :"<<num;
+
+}
